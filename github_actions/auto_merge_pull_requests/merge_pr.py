77162d102a3aa212bef878e3d3c0f66ad68064e4
Ryan Casperson <ryan.casperson@jumpcloud.com>
2019-11-06 16:28:04 -0700

    Try github comment action

--- github_actions/auto_merge_pull_requests/merge_pr.py
+++ github_actions/auto_merge_pull_requests/merge_pr.py
@@ -2,30 +2,8 @@
 import sys
 import os
 
-import requests
+GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
 
-
-def create_session(github_token):
-    sess = requests.Session()
-    sess.headers = {
-        "Accept": "; ".join([
-            "application/vnd.github.v3+json",
-            "application/vnd.github.antiope-preview+json",
-        ]),
-        "Authorization": f"token {github_token}",
-        "User-Agent": f"GitHub Actions script in {__file__}"
-    }
-
-    def raise_for_status(resp, *args, **kwargs):
-        try:
-            resp.raise_for_status()
-        except Exception:
-            print(resp.text)
-            sys.exit("Error: Invalid repo, token or network issue!")
-
-    sess.hooks["response"].append(raise_for_status)
-    return sess
 
 if __name__ == "__main__":
     print("Hello world!")
-    sess = create_session(os.environ["GITHUB_TOKEN"])
